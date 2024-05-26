bade769aaa92a4f3e31dd1d42e32170fbeffae76
akhil1999 <akhileshbhuvanendra@gmail.com>
2024-05-26 15:24:52 +0530

    Update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export clang-triple = android/prebuilts/clang/host/linux-x86/clang-4639204/bin/aarch64-linux-gnu-
+PWD=$(pwd)
+export CROSS_COMPILE=$PWD/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CLANG_TRIPLE=$PWD/toolchain/clang-4639204/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=r
 export PLATFORM_VERSION=11
-
 make exynos9610-a50sxx_defconfig
-make -j64
+make -j12 \
+	CC=$PWD/toolchain/clang-4639204/bin/clang \
+	CROSS_COMPILE=$PWD/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
